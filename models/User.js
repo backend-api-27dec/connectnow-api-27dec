440364b16b311ce72065f8570c0f940387d1067b
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-07-24 13:39:37 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,7 +1,8 @@
-    const mongoose = require('mongoose');
-    const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-    const userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema(
+    {
         name: {
             type: String,
             required: true,
@@ -15,13 +16,26 @@
             type: String,
             required: true,
         },
-    }, {
+    },
+    {
         timestamps: true,
-    });
+    }
+);
 
-    userSchema.methods.matchPassword = async function (enteredPassword) {
-        return await bcrypt.compare(enteredPassword, this.password);
-    };
+// Hash the password before saving
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
 
-    const User = mongoose.model('User', userSchema);
-    module.exports = User;
+// Add a method to check if the entered password matches the hashed password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
