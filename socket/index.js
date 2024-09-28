5b5192cfddb53fdc9a65931fab0b5bb62d30b4f3
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-09-28 20:22:47 +0530

    Update index.js

--- socket/index.js
+++ socket/index.js
@@ -6,7 +6,7 @@ module.exports = (io) => {
     let currentUser = null;
 
     socket.on('joinRoom', ({ room, user }) => {
-      console.log(${user} joined room: ${room});
+      console.log(`${user} joined room: ${room}`);
       socket.join(room);
       currentRoom = room;
       currentUser = user;
